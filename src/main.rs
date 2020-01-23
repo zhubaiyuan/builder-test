b5a88754eb7672ff4b896a9f06cfff90c4d2efdd
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2020-01-23 12:59:32 +0800

    import builder macro, define struct X and struct Item

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use builder::Builder;
+
+#[derive(Debug)]
+struct X {}
+
+#[derive(Debug, Builder)]
+struct Item<T, U>
+where
+    T: Default,
+{
+    a: u32,
+    b: Option<&'static str>,
+    c: String,
+    #[builder(required)]
+    d: X,
+    e: T,
+    #[builder(required)]
+    f: U,
 }
